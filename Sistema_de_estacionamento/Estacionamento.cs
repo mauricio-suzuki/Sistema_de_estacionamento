02c95c75a8351487f69be3c493d8b445ed13355f
José Mauricio Suzuki Portella <craftgutin@gmail.com>
2024-02-09 20:34:29 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema_de_estacionamento/Estacionamento.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_estacionamento
+{
+    public class Estacionamento
+    {
+        public List<string> veiculos = new List<string>();
+        public string placa { get; set; }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo");
+            veiculos.Add(Console.ReadLine());
+            Console.WriteLine("Veículo adicionado com sucesso");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void ListarVeículos()
+        {
+            Console.WriteLine("Lista de veículos:");
+            foreach (string veiculo in veiculos)
+            {
+                Console.WriteLine(veiculo);
+
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void RemoverVeículo()
+        {
+            Console.WriteLine("Lista de veículos:");
+            foreach (string veiculo in veiculos)
+            {
+                Console.WriteLine(veiculo);
+
+            }
+            Console.WriteLine("Digite a placa do veículo");
+            placa = Console.ReadLine();
+
+            if (veiculos.Contains(placa))
+            {
+                veiculos.Remove(placa);
+                Console.WriteLine("Veículo removido com sucesso!");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            else
+            {
+                Console.WriteLine("Veículo não encontrado");
+                Console.Clear();
+            }
+        }
+
+        public void QtdVeiculo()
+        {
+            for (int i = 0; i < veiculos.Count; i++)
+            {
+                Console.WriteLine($"Veiculo{i + 1}: {veiculos[i]}");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+
+        }
+    }
+}
---
+++ Sistema_de_estacionamento/Program.cs
@@ -0,0 +1,41 @@
+﻿using Sistema_de_estacionamento;
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        int opcao;
+       Estacionamento estacionamento = new Estacionamento();
+
+        Console.WriteLine("==============================");
+        Console.WriteLine("BEM-VINDO AO ESTACIONAMENTO X");
+        Console.WriteLine("==============================");
+
+        do
+        {  
+            Console.WriteLine("1 - Adicionar veículo");
+            Console.WriteLine("2 - Listar veículos");
+            Console.WriteLine("3 - Quantidade de veículos");
+            Console.WriteLine("4 - Remover veículo");
+            Console.WriteLine("5 - Sair");
+            opcao = int.Parse(Console.ReadLine());
+            Console.Clear();
+            switch (opcao)
+            {
+               
+                 case 1: estacionamento.AdicionarVeiculo();
+                      break;
+                 case 2: estacionamento.ListarVeículos();
+                      break;
+                case 3: estacionamento.QtdVeiculo(); 
+                      break;
+                case 4: estacionamento.RemoverVeículo();
+                     break;
+            }
+
+
+        } while (opcao != 5);
+
+    }
+        
+}
